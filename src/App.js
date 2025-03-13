45ab4facffae26455d8b552a7a92184e90f96cd6
vitcool@ukr.net <vitcool@ukr.net>
2025-03-13 12:00:33 +0200

    DK-0: console.log is added to test build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('Hello, Docker!!! This is a React App running in a Docker container.');
   return (
     <div className="App">
       <header className="App-header">
